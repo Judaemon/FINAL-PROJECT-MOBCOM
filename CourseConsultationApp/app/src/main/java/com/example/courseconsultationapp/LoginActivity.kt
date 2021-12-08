4247324b60f91bd240a62a9c1c7ff0b9f5d92776
Arellano Hanz Mark <69075813+bskiller211@users.noreply.github.com>
2021-12-08 17:58:55 +0800

    Login Working na

--- CourseConsultationApp/app/src/main/java/com/example/courseconsultationapp/LoginActivity.kt
+++ CourseConsultationApp/app/src/main/java/com/example/courseconsultationapp/LoginActivity.kt
@@ -3,8 +3,10 @@ package com.example.courseconsultationapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -35,6 +37,23 @@ class LoginActivity : AppCompatActivity() {
                 emailValue = email.editText?.text.toString()
                 passwordValue = password.editText?.text.toString()
 
+                auth.signInWithEmailAndPassword(emailValue, passwordValue)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success")
+                            val user = auth.currentUser
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(baseContext, "Wrong Email or password.",
+                                Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
 //                createAccount(emailValue, passwordValue)
             }
         }
@@ -43,6 +62,50 @@ class LoginActivity : AppCompatActivity() {
         textRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
             finish()
+        }
+    }
+
+    private fun validateEmail(): Boolean {
+        val newEmail = email.editText?.text.toString().trim { it <= ' ' }
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex()
+
+        return if (newEmail.isEmpty()) {
+            email.error = "Required field"
+            false
+        } else if (!newEmail.matches(emailPattern)) {
+            email.error = "Invalid email address"
+            false
+        } else {
+            email.error = null
+            email.isErrorEnabled = false
+            true
+        }
+    }
+
+    private fun validatePassword(): Boolean {
+        val newPassword = password.editText?.text.toString().trim { it <= ' ' }
+
+        val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
+        val passwordMatcher = Regex(passwordPattern)
+
+        return when {
+            newPassword.isEmpty() -> {
+                password.error = "Required field"
+                false
+            }
+            passwordMatcher.find(newPassword) != null -> {
+                password.error = "Password is too weak"
+                false
+            }
+            newPassword.length <= 8 -> {
+                password.error = "Password too short"
+                false
+            }
+            else -> {
+                password.error = null
+                password.isErrorEnabled = false
+                true
+            }
         }
     }
 
